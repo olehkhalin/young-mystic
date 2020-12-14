b981aeacfabcad29951793f7cdcc86c22af28585
olehkhalin <olehkhalin@gmail.com>
2020-12-14 16:47:09 +0200

    Some secondary pages

--- src/components/products/Filters/Filters.tsx
+++ src/components/products/Filters/Filters.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cx from 'classnames';
 
+import { FilterScreen } from '@components/products/FilterScreen';
 import ChewronDown from '@public/svg/ChewronDown.svg';
 
 import s from './Filters.module.sass';
@@ -11,21 +12,32 @@ type FiltersProps = {
 
 export const Filters: React.FC<FiltersProps> = ({
   className,
-}) => (
-  <div className={cx(s.root, className)}>
-    <button
-      type="button"
-      className={s.button}
-    >
-      Выбрать по
-      <ChewronDown className={s.icon} />
-    </button>
-    <button
-      type="button"
-      className={cx(s.button, s.sort)}
-    >
-      Сортировать
-      <ChewronDown className={s.icon} />
-    </button>
-  </div>
-);
+}) => {
+  const [isFiltersOpen, setIsFiltersOpen] = useState(false);
+
+  return (
+    <>
+      <div className={cx(s.root, className)}>
+        <button
+          type="button"
+          className={s.button}
+          onClick={() => setIsFiltersOpen(true)}
+        >
+          Выбрать по
+          <ChewronDown className={s.icon} />
+        </button>
+        <button
+          type="button"
+          className={cx(s.button, s.sort)}
+        >
+          Сортировать
+          <ChewronDown className={s.icon} />
+        </button>
+      </div>
+      <FilterScreen
+        onRequestClose={() => setIsFiltersOpen(false)}
+        className={cx(s.filtersScreen, { [s.open]: isFiltersOpen })}
+      />
+    </>
+  );
+};
