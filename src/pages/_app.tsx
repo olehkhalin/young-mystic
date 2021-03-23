25a2c73455f66f9c6172bc5e40d46c317846a3dc
olehkhalin <olehkhalin@gmail.com>
2021-03-23 19:14:38 +0200

    Updated dependencies and fixed errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { AppProps } from 'next/app';
 
 import { withApollo } from '@client';
-import { appWithTranslation } from '@i18n';
+import { appWithTranslation } from 'next-i18next';
 import '@styles/globals.sass';
 
-function MyApp({ Component, pageProps }: any) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
